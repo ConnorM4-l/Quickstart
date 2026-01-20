2aae9e4675838758d9fdd67a2f1a7475f6889593
ConnorM4-l <connorgmck@gmail.com>
2026-01-20 15:24:40 -0600

    updated camera logic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/DecideOrder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/DecideOrder.java
@@ -2,9 +2,48 @@ package org.firstinspires.ftc.teamcode.util;
 
 public class DecideOrder {
     //this will return the order in which to shoot
-    public void order(String motif, int positionGreen) {
-        if (motif.equals("GPP")) {
+    /*
+    motif:
+    Blue Goal: 20
+    Motif GPP: 21
+    Motif PGP: 22
+    Motif PPG: 23
+    Red Goal: 24
 
+    positionGreen:
+    1 = left
+    2 = right
+    3 = back
+
+    return:
+    1: LL
+    2: RR
+    3: LR
+    4: RL
+     */
+    public int order(int motif, int positionGreen) {
+        if (motif == 21) {
+            if (positionGreen == 1) {
+                return 3;
+            } else  {
+                return 4;
+            }
+        } else if (motif == 22) {
+            if (positionGreen == 1) {
+                return 4;
+            } else if (positionGreen == 3) {
+                return 2;
+            } else {
+                return 3;
+            }
+        } else {
+            if (positionGreen == 1) {
+                return 2;
+            } else if (positionGreen == 2) {
+                return 1;
+            } else {
+                return 3;
+            }
         }
     }
 }
