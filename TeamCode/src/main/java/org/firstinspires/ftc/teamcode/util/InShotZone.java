0b812f71cb4d609340252d7509135788defd45a0
ConnorM4-l <connorgmck@gmail.com>
2026-01-25 13:00:34 -0600

    in shot zone class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/InShotZone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/InShotZone.java
@@ -1,4 +1,24 @@
 package org.firstinspires.ftc.teamcode.util;
 
+import com.pedropathing.geometry.Pose;
+
 public class InShotZone {
+    private Pose currentPose;
+
+    public InShotZone() {
+        currentPose = new Pose();
+    }
+
+    public void setCurrentPose(Pose pose) {
+        currentPose = pose;
+    }
+
+    public boolean isInShotZone() {
+        double x = currentPose.getX();
+        double y = currentPose.getY();
+
+
+
+    }
+
 }
